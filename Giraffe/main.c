e763f306b4d42dfdcf11ff7f2d98cf21a9ff6b1c
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2023-10-08 16:58:49 +0200

    first commit

---
+++ Giraffe/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* char name[] = "huj";
+    printf("%s\n", name);
+    */
+
+    int age;
+    scanf("%d", &age);
+    printf("%d", age);
+
+    return 0;
+}

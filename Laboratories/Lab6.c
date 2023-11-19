3e185b8886725c90a3b1f4e58c9de4317c10b7bd
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2023-11-19 20:55:20 +0100

    excercise for sixth lab

---
+++ Laboratories/Lab6.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define TRDAYS 500
+#define PLAYERNR 3
+
+float* trade_game(int numPlayers, float tradeHistory[]);
+void bubble_sort(float array[], int length);
+
+int main(void) {
+    srand((unsigned int)time(NULL));
+
+    float history[TRDAYS];
+    history[0] = 13.9;
+    
+    float max = history[0];
+    float min = history[0];
+
+    for (int i = 1; i < TRDAYS + 1; ++i)
+    {
+        float arbitrary_number = rand() % (110 - 90 + 1) + 90;
+        history[i] = arbitrary_number * history[i - 1] / 100;
+        //printf("Day: %d, Value: %f \n", i, history[i-1]);
+
+        if (max < history[i])
+        {
+            max = history[i];
+        }
+        if (min > history[i])
+        {
+            min = history[i];
+        }
+        
+        
+    }
+
+    printf("Max value: %f, Min value: %f \n", max, min);
+
+    float *wealthOfPlayers = trade_game(PLAYERNR, history);
+    for (int j = 0; j < PLAYERNR; ++j)
+    {
+        float result = wealthOfPlayers[j];
+        printf("Player: %d, Wealth: %f \n", j+1, result);
+    }
+    
+    int length = PLAYERNR;
+
+    bubble_sort(wealthOfPlayers, length);
+
+    free(wealthOfPlayers);
+    
+    return 0;
+}
+
+float* trade_game(int numPlayers, float tradeHistory[]){
+    
+    srand((unsigned int)time(NULL));
+    
+    float wealths[PLAYERNR];
+    float valuesOftocks[PLAYERNR];
+
+    /*For each player*/
+    for (int i = 0; i < PLAYERNR; i++) {
+        wealths[i] = 100;
+        valuesOftocks[i] = 0;
+
+        /*We go through every day of operation on a stock market*/
+        for (int j = 0; j < TRDAYS; ++j)
+        {
+            char operations[] = {'B', 'S'};
+            int size = sizeof(operations) / sizeof(operations[0]);
+            int randomIndex = rand() % size;
+            char operation = operations[randomIndex];
+
+            if (operation == 'S')    //case player sell a stock
+            {
+                float soldStock = fmod(rand(), valuesOftocks[i] + 1);
+                if (valuesOftocks[i] >= soldStock)
+                {
+                    wealths[i] += soldStock;
+                    valuesOftocks[i] -= soldStock;
+                }
+
+            }
+            else if (operation == 'B')   //case player buy a stock
+            {
+                float boughtStock = fmod(rand(), tradeHistory[j] + 1);
+                if (wealths[i] >= boughtStock)
+                {
+                    wealths[i] -= boughtStock;
+                    valuesOftocks[i] += boughtStock;
+                }
+                
+            }
+            
+
+        }
+        
+    }
+    /*At the end we add the value of stocks
+    to value of money for each player*/
+    for (int k = 0; k < PLAYERNR; ++k)
+    {
+        wealths[k] += valuesOftocks[k];
+    }
+    
+    // Return a pointer to the array
+    float *result = malloc(PLAYERNR * sizeof(float));
+    for (int k = 0; k < PLAYERNR; ++k) {
+        result[k] = wealths[k];
+    }
+    return result;
+    
+}
+
+void bubble_sort(float array[], int length){
+
+  for (int step = 0; step < length - 1; ++step) {
+    int swapped = 0;
+
+    for (int i = 0; i < length - step - 1; ++i) {
+      if (array[i] > array[i + 1]) {
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = 1;
+      }
+    }
+    
+    if (swapped == 0) {
+      break;
+    }
+  }
+  printf("A sorted list of players' wealth\n");
+  for (int i = 0; i < length; ++i) {
+    printf("%f  ", array[i]);
+  }
+    float max = array[0];
+    float min = array[0];
+  for (int j = 0; j < length; ++j) {
+    if (max < array[j])
+        {
+            max = array[j];
+        }
+        if (min > array[j])
+        {
+            min = array[j];
+        }
+  }
+  printf("\nRichest player account : %f, Poorest player account: %f \n", max, min);
+}
